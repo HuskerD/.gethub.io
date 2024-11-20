0699f67786c60a796c22bee58b2659cd95d8c2ce
HuskerD <163834983+HuskerD@users.noreply.github.com>
2024-11-20 04:27:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMdXp00iA-dpIob0jA6bIrpb42X1atWus",
+  authDomain: "fir-firestore-4a196.firebaseapp.com",
+  projectId: "fir-firestore-4a196",
+  storageBucket: "fir-firestore-4a196.appspot.com",
+  messagingSenderId: "55908652697",
+  appId: "1:55908652697:web:95d44df09607e3bf544a08",
+  measurementId: "G-E5884RT99B"
